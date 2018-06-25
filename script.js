6a4376d6161238e4c58883c0a931dc2672ad7954
kevin <kevinelsen@hotmail.fr>
2018-06-25 11:54:24 +0200

    exo5

---
+++ script.js
@@ -0,0 +1,25 @@
+/*Déclaration de la foncion*/
+function img1() {
+/*Changement d'images*/
+document.getElementById('image1').src = 'assets/img/image1_2.jpg';
+}
+/*Déclaration de la foncion*/
+function img2() {
+/*Changement d'images*/
+document.getElementById('image2').src = 'assets/img/image2_2.jpg';
+}
+/*Déclaration de la foncion*/
+function img3() {
+/*Changement d'images*/
+document.getElementById('image3').src = 'assets/img/image3_2.jpg';
+}
+/*Déclaration de la foncion*/
+function img4() {
+/*Changement d'images*/
+document.getElementById('image4').src = 'assets/img/image4_2.jpg';
+}
+/*Déclaration de la foncion*/
+function img5() {
+/*Changement d'images*/
+document.getElementById('image5').src = 'assets/img/image5_2.jpg';
+}
